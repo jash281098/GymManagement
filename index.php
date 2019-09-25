8d2b82231ff61bbfeb1a0d8ba635df9a01f683fa
jash281098 <jashmehta200@gmail.com>
2019-09-25 16:31:37 +0530

    GA

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-148732250-1"></script>
+`		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-148732250-1');
+		</script>
 		<meta charset="utf-8">
 		<title>My Gym</title>
 		<link href="style.css" rel="stylesheet" type="text/css">
